91323725beefd165081abcd2c2c478b135ecc91f
nafisa <nafisatasnim8855@gmail.com>
2024-10-08 11:36:32 +0600

    modified OrderManager

--- src/task1/OrderManager.java
+++ src/task1/OrderManager.java
@@ -1,9 +1,28 @@
 package task1;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class OrderManager {
     List<Product> productList;
 
+    OrderManager(List<Product> products){
+        this.productList = products;
+    }
+
+    public void sortProducts()
+    {
+        productList.sort(Comparator.comparing(Product::totalPrice).thenComparing(Product::getName).thenComparing(Product::getStockQuantity));
+
+    }
+
+    public void showProducts()
+    {
+        for(Product product:productList){
+            System.out.println("Product Name:" + product.getName()
+                    + "Product Price:" + product.totalPrice()
+                    + "Product Quantity:" + product.getStockQuantity());
+        }
+    }
 
 }
